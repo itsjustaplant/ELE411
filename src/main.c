1dc87597768989012a9f98a4c03156fda6274dca
itsjustaplant <alperensahin442@gmail.com>
2020-09-25 00:48:42 +0300

    More fixes to project structure

--- src/main.c
+++ src/main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-#include "../queue/include/queue.h"
-
+#include "../libs/queue/include/queue.h"
 int main() {
 
     struct Queue* queue = BuildQueue();
     for (int i = 0; i < 10; ++i) {
         Enqueue(i,queue);
-    }
-    for (int i = 0; i < 10; ++i) {
-        printf("%d\n",queue -> buffer[i]);
-    }
-    for (int i = 0; i < 10; ++i) {
-        Dequeue(queue);
     }
     free(queue -> buffer);
     free(queue);
